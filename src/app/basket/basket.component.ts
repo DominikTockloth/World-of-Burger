c024a7257c947050b2e63eb97ddb3a050c03fdf9
DominikTockloth <128898734+DominikTockloth@users.noreply.github.com>
2024-11-20 17:04:08 +0100

    added sticky style to basket component

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-basket',
@@ -14,5 +14,24 @@ import { Component } from '@angular/core';
 export class BasketComponent {
   isBasketEmpty: boolean = false;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const basket = this.el.nativeElement.querySelector('#basketWrapper');
+    const basketBox = this.el.nativeElement.querySelector('#basketBox');
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollPosition > 80) { // Passe den Wert an, ab wann der Basket fixed sein soll
+      this.renderer.setStyle(basket, 'position', 'fixed');
+      this.renderer.setStyle(basket, 'top', '0');
+      this.renderer.setStyle(basket, 'right', '0');
+      this.renderer.setStyle(basket, 'z-index', '4');
+      this.renderer.setStyle(basketBox, 'height', '100vh');
+
+    } else {
+      this.renderer.setStyle(basket, 'position', 'absolute');
+      this.renderer.setStyle(basket, 'top', '80px'); // Ursprüngliche Position
+    }
+  }
 }
