a21bddf14a5905659e50efe97f82657a5975b075
DominikTockloth <128898734+DominikTockloth@users.noreply.github.com>
2024-11-25 09:12:40 +0100

    added icon for payment method

--- src/app/overlays/components/page-information/page-information.component.ts
+++ src/app/overlays/components/page-information/page-information.component.ts
@@ -1,13 +1,15 @@
+import { NgFor } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-page-information',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './page-information.component.html',
   styleUrl: './page-information.component.scss'
 })
 export class PageInformationComponent {
+  paymentImages: string[] = ['/images/card.png', '/images/bitcoin.png', '/images/contactless.png', '/images/pound.png', '/images/euro.png', '/images/rupee.png', '/images/ruble.png',];
   isPageInfoOpen: boolean = false;
 
   openPageInfo(): void {
